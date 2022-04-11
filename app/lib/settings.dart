0439361c4110914e7d7f56ef1d1a77b7f03aac80
Shiv <shivdeepak936@gmail.com>
2022-04-11 23:47:32 +0530

    Settings page updated

--- app/lib/settings.dart
+++ app/lib/settings.dart
@@ -6,26 +6,73 @@ class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: const Color(0xFF032B49),
       body: Container(
             child: Column(children: <Widget>[
               Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: const [
                   Icon(Icons.account_circle_outlined,
-                      size: 250.0, semanticLabel: "User Profile"),
+                      size: 250.0, semanticLabel: "User Profile",color: Colors.blueAccent),
                 ],
               ),
-              Row(
+              Column(
                 children: [
+                  Container(
+                    padding: const EdgeInsets.all(15.0),
+                    margin: const EdgeInsets.all(20.0),
+                    height: 100,
+                    width: 250,
+                    decoration: BoxDecoration(
+                        color: Colors.blue[200],
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.blue,
+                            offset: Offset(4, 4),
+                            blurRadius: 10,
+                            spreadRadius: 1,
+                          )]
+                    ),
+                    child: FittedBox(
+                      fit: BoxFit.scaleDown,
+                      child: Text(
+                        ('User Id: 264867'),
+                        style: TextStyle(
+                          color: Colors.grey[900],
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
                 Container(
-                   color: Colors.blue,
-                   margin: const EdgeInsets.all(25.0),
-                   child: const Text('User Id: 264867',)
-                ),
-                Container(
-                   color: Colors.blue,
-                   margin: const EdgeInsets.all(25.0),
-                   child: const Text('User Id: 264867')
+                    padding: const EdgeInsets.all(15.0),
+                    margin: const EdgeInsets.all(20.0),
+                    height: 100,
+                    width: 250,
+                    decoration: BoxDecoration(
+                        color: Colors.blue[200],
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.blue,
+                            offset: Offset(4, 4),
+                            blurRadius: 10,
+                            spreadRadius: 1,
+                          )]
+                    ),
+                    child: FittedBox(
+                      fit: BoxFit.scaleDown,
+                      child: Text(
+                        ('User Name: AbcdWxyz'),
+                        style: TextStyle(
+                          color: Colors.grey[900],
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
                 ),
                 ]
               ),
