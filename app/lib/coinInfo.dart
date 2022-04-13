3d8e754d0f4c83a8cedc7a165a1b7ddb4ab0a7ee
suraj429 <suraj7b@gmail.com>
2022-04-13 19:53:31 +0530

    changed label of x axis in line graph

--- app/lib/coinInfo.dart
+++ app/lib/coinInfo.dart
@@ -24,15 +24,6 @@ class _CoinInfoState extends State<CoinInfo>{
       title: 'Crypto Live',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -43,14 +34,6 @@ class _CoinInfoState extends State<CoinInfo>{
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -65,7 +48,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     chartData = getChartData();
-    Timer.periodic(const Duration(seconds: 1), updateDataSource);
+    // Timer.periodic(const Duration(seconds: 1), updateDataSource);
     super.initState();
   }
 
@@ -75,12 +58,12 @@ class _MyHomePageState extends State<MyHomePage> {
     return SafeArea(
         child: Scaffold(
             body: SfCartesianChart(
-              title: ChartTitle(text: coinList[0].name,
+                title: ChartTitle(text: coinList[0].name,
                     borderWidth: 2,
                     textStyle: TextStyle(
-                    fontSize: 20,
-                )
-              ),
+                      fontSize: 20,
+                    )
+                ),
                 series: <LineSeries<LiveData, int>>[
                   LineSeries<LiveData, int>(
                     onRendererCreated: (ChartSeriesController controller) {
@@ -95,12 +78,16 @@ class _MyHomePageState extends State<MyHomePage> {
                 primaryXAxis: NumericAxis(
                     majorGridLines: const MajorGridLines(width: 0),
                     edgeLabelPlacement: EdgeLabelPlacement.shift,
-                    interval: 3,
-                    title: AxisTitle(text: 'Months')),
+                    interval: 1,
+                    title: AxisTitle(text: 'Hour')),
                 primaryYAxis: NumericAxis(
                     axisLine: const AxisLine(width: 0),
                     majorTickLines: const MajorTickLines(size: 0),
-                    title: AxisTitle(text: 'Market Price')))));
+                    title: AxisTitle(text: 'Market Price')
+                )
+            ),
+        )
+    );
   }
 
   int time = 19;
@@ -141,4 +128,3 @@ class LiveData {
   final int time;
   final num speed;
 }
-
