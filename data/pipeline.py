3dcfa363ad3d3c89be9ab11bd5c977b09313a30c
Sumit <diysumit@gmail.com>
2022-08-27 23:35:07 +0530

    Bug fixes and Implementations - modified or added import statements - changed coin list - changed doc strings - added bunch of new keys - changed some comments, added warning about api data skew - implemented save_data function - added try-catch block in main method

--- data/pipeline.py
+++ data/pipeline.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import traceback
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
 from apache_beam.dataframe.io import read_json, read_csv
@@ -8,7 +9,8 @@ from apache_beam.dataframe.convert import to_dataframe, to_pcollection
 import argparse
 import requests
 import sys
-import modin.pandas as pd
+import json
+import pandas as pd
 
 from datetime import date, timedelta
 
@@ -26,8 +28,8 @@ output_path = arguments.output_path
 runner = arguments.runner
 
 #* coin list
-coins = {'btcusd': ['BTC/USD', './unprocessed_data/btcusd.csv'],
-         'ethusd': ['ETH/USD', './unprocessed_data/ethusd.csv']}
+coins = {'btcusd': ['BTC/USD', 'btcusd.csv'],
+         'ethusd': ['ETH/USD', 'ethusd.csv']}
 
 #* create beam pipeline options
 #//// TODO: Implementation Required
@@ -35,7 +37,7 @@ def create_options() -> PipelineOptions:
       """
       def create_options() -> PipelineOptions:
 
-            return beam pipeline options object
+            return beam PipelineOptions object
       """
       options = PipelineOptions(['--runner', runner])
       return options
@@ -44,32 +46,29 @@ def create_options() -> PipelineOptions:
 #//// TODO: Implementation Required
 def get_data(coin: list) -> requests.Response.text:
     """
-        def get_data(coin: list) -> None
+        def get_data(coin: list) -> requests.Response.text
 
             returns json_obj
     """
     key = 'FAA10B2B-C744-43C1-90BC-D1D0A0007E4C'
+# key = '3E0B48CC-DCD8-4A5C-B336-8DCD7F024521'
+# key = '277A3033-99A9-47AE-AC2F-6C9E557F47B0'
+# key = 'CD276776-814F-4E42-A603-1A97E774AB36'
+# key = '133B3720-92C1-49FA-8B14-E6CFBB53EC7F'
+#     key = '0ADD391A-E5EB-4991-9E08-ECEAA0A2958A'
     headers = {'X-CoinAPI-Key' : key}
     date_today = date.today()
-    start_date = date_today - timedelta(days=365) # 7 years of data
-    url = f"https://rest.coinapi.io/v1/exchangerate/{coin[0]}/history?period_id=1HRS&time_start={start_date}T00:00:00&time_end={date_today}T00:00:00&limit=100000"
+    start_date = date_today - timedelta(days=365) # 1 years of data
+    #! the api response it still skewed, need fixing
+    url = f"https://rest.coinapi.io/v1/exchangerate/{coin[0]}/history?period_id=1HRS&time_start={start_date}T00:00:00&time_end={date_today}T00:00:00"
     response = requests.get(url=url, headers=headers)
     return response.text
 
-#* Writes PTransform to csv
-def write_to_csv(data: beam.PTransform):
-      pass
-
 #* Saves json objects locally in csv_files
-# TODO: Implementation Required
-def save_data(data, path):
-      P = beam.Pipeline()
-      df = (
-            P
-            | "Read Json Objects" >> read_json(data)
-            | "Convert PTransform to DataFrame" >> to_dataframe() #! doesn't work api response is skewed
-            )
-      df.to_csv(path)
+#//// TODO: Implementation Required
+def save_data(data, filename):
+      data = pd.DataFrame.from_dict([json.loads(data)])
+      data.to_csv(f"{input_path}{filename}")
 
 # TODO: Implementation Required
 def save_results():
@@ -81,17 +80,22 @@ def run(OPTIONS: PipelineOptions, input_path: str):
             (
             P 
             | "Read CSV files" >> read_csv(input_path)
+            | "Convert to dataframe" >> to_dataframe()
             | "Save results" >> beam.Map(save_results())
             )
 
 def main():
-      unprocessed_data = []
-      for coin in coins:
-            unprocessed_data.append(get_data(coin))
-      OPTIONS = create_options()
-      for json_obj, coin in zip(unprocessed_data, coins):
-            save_data(data=json_obj, path=coin[1])
-      run(OPTIONS=OPTIONS, input_path=input_path)
+      try:
+            unprocessed_data = []
+            for coin in coins:
+                  unprocessed_data.append(get_data(coin))
+            OPTIONS = create_options()
+            for json_obj, coin in zip(unprocessed_data, coins):
+                  save_data(data=json_obj, filename=coins.get(coin)[1])
+            run(OPTIONS=OPTIONS, input_path=input_path)
+     
+      except Exception as e:
+            traceback.print_exc(e)
 
 if __name__ == "__main__":
       sys.exit(main())
