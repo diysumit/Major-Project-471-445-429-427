64ddbad01648a8589ae3010dcb3148f4773e2bed
Sumit <diysumit@gmail.com>
2022-08-28 20:47:11 +0530

    Bug Fixes - Fixed api call - Added keys - Changed save_data() implementation - Added column names - other small fixes

--- data/pipeline.py
+++ data/pipeline.py
@@ -10,6 +10,7 @@ import argparse
 import requests
 import sys
 import json
+import csv
 import pandas as pd
 
 pd.options.plotting.backend = 'plotly'
@@ -28,6 +29,16 @@ arguments = parser.parse_args()
 input_path = arguments.input_path
 output_path = arguments.output_path
 runner = arguments.runner
+
+#* column list
+col_names = ['time_period_start',
+             'time_period_end', 
+             'time_open', 
+             'time_close', 
+             'rate_open', 
+             'rate_high', 
+             'rate_low', 
+             'rate_close']
 
 #* coin list
 coins = {'btcusd': ['BTC/USD', 'btcusd.csv'],
@@ -52,26 +63,33 @@ def get_data(coin: list) -> requests.Response.text:
 
             returns json_obj
     """
-    key = 'FAA10B2B-C744-43C1-90BC-D1D0A0007E4C'
-# key = '3E0B48CC-DCD8-4A5C-B336-8DCD7F024521'
-# key = '277A3033-99A9-47AE-AC2F-6C9E557F47B0'
-# key = 'CD276776-814F-4E42-A603-1A97E774AB36'
-# key = '133B3720-92C1-49FA-8B14-E6CFBB53EC7F'
+#     key = 'FAA10B2B-C744-43C1-90BC-D1D0A0007E4C'
+#     key = '3E0B48CC-DCD8-4A5C-B336-8DCD7F024521'
+#     key = '277A3033-99A9-47AE-AC2F-6C9E557F47B0'
+#     key = 'CD276776-814F-4E42-A603-1A97E774AB36'
+#     key = '133B3720-92C1-49FA-8B14-E6CFBB53EC7F'
 #     key = '0ADD391A-E5EB-4991-9E08-ECEAA0A2958A'
+#     key = 'FAA10B2B-C744-43C1-90BC-D1D0A0007E4C'
+    key = 'B9F26C13-0CAB-4172-9392-5F9BF60AC75C'
+
     headers = {'X-CoinAPI-Key' : key}
     date_today = date.today()
     start_date = date_today - timedelta(days=365) #* 1 years of data
 
-    #! FIX THIS: the api response it still skewed, need fixing
-    url = f"https://rest.coinapi.io/v1/exchangerate/{coin[0]}/history?period_id=1HRS&time_start={start_date}T00:00:00&time_end={date_today}T00:00:00"
+    #!//// FIX THIS: the api response it still skewed, need fixing
+    url = f"https://rest.coinapi.io/v1/exchangerate/{coin[0]}/history?period_id=1HRS&time_start={start_date}T00:00:00&time_end={date_today}T00:00:00&limit=100000"
     response = requests.get(url=url, headers=headers)
     return response.text
 
 #* Saves json objects locally in csv_files
 #//// TODO: Implementation Required
-def save_data(data, filename) -> None:
-      data = pd.DataFrame.from_dict([json.loads(data)])
-      data.to_csv(f"{input_path}{filename}")
+def save_data(data: list, filename: str) -> None:
+      list_of_dicts = json.loads(data)
+      with open(file=input_path+filename) as csv_file:
+            dict_writer = csv.DictWriter(csv_file, fieldnames=col_names)
+            dict_writer.writeheader()
+            for dictionary in list_of_dicts:
+                  dict_writer.writerow(dictionary)
 
 # TODO: Implementation Required
 def save_results(df: pd.DataFrame) -> None:
@@ -93,7 +111,7 @@ def main() -> None:
       try:
             unprocessed_data = []
             for coin in coins:
-                  unprocessed_data.append(get_data(coin))
+                  unprocessed_data.append(get_data(coins.get(coin)))
             OPTIONS = create_options()
             for json_obj, coin in zip(unprocessed_data, coins):
                   filename = coins.get(coin)[1]
