00fcc7c6d521228977062226e5b6b89f2691085c
Sumit <diysumit@gmail.com>
2022-08-30 21:25:07 +0530

    Implementation of pipeline.py complete - The required implementation of pipeline.py complete, it copies and saves data as required by the project - Added pipe to export data to BigQuery, didn't check implementation, will test later - Moved/Copied files not needed to 'old' folder - Will modify bash script and add new model to run with new pipeline.

--- data/pipeline.py
+++ data/pipeline.py
@@ -3,25 +3,29 @@
 import traceback
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
-from apache_beam.dataframe.io import read_json, read_csv
-from apache_beam.dataframe.convert import to_dataframe, to_pcollection
+from apache_beam.dataframe.io import read_csv
+from apache_beam.dataframe.convert import to_pcollection
 
 import argparse
 import requests
 import sys
 import json
 import csv
-import pandas as pd
 
-pd.options.plotting.backend = 'plotly'
+# pd.options.plotting.backend = 'plotly' #! Plotting tools not supported yet
 
 from datetime import date, timedelta
 
-parser = argparse.ArgumentParser(description="")
+parser = argparse.ArgumentParser(description="Save data from CoinAPI")
 
-parser.add_argument('--runner', required=True, help='Specify Apache Beam Runner')
+parser.add_argument('--runner', required=False, help='Specify Apache Beam Runner')
 parser.add_argument('--input_path', required=True, help='Path to CSV file')
 parser.add_argument('--output_path', required=True, help='Path to save results')
+parser.add_argument('--project',required=False, help='Specify Google Cloud project')
+parser.add_argument('--region', required=False, help='Specify Google Cloud region')
+parser.add_argument('--staging_location', required=False, help='Specify Cloud Storage bucket for staging')
+parser.add_argument('--temp_location', required=False, help='Specify Cloud Storage bucket for temp')
+parser.add_argument('--raw_table_name', required=False, help='BigQuery table name for saving raw results')
 
 arguments = parser.parse_args()
 
@@ -29,6 +33,11 @@ arguments = parser.parse_args()
 input_path = arguments.input_path
 output_path = arguments.output_path
 runner = arguments.runner
+project = arguments.project
+region = arguments.region
+staging_location = arguments.staging_location
+temp_location = arguments.temp_location
+raw_table_name = arguments.raw_table_name
 
 #* column list
 col_names = ['time_period_start',
@@ -52,7 +61,10 @@ def create_options() -> PipelineOptions:
 
             return beam PipelineOptions object
       """
-      options = PipelineOptions(['--runner', runner])
+      if runner and project and region and temp_location:
+            options = PipelineOptions(['--runner', runner, '--project', project, '--region', region, '--temp_location', temp_location])
+      else:
+            options = PipelineOptions()
       return options
 
 #* get response from api and return json_obj
@@ -70,7 +82,9 @@ def get_data(coin: list) -> requests.Response.text:
 #     key = '133B3720-92C1-49FA-8B14-E6CFBB53EC7F'
 #     key = '0ADD391A-E5EB-4991-9E08-ECEAA0A2958A'
 #     key = 'FAA10B2B-C744-43C1-90BC-D1D0A0007E4C'
-    key = 'B9F26C13-0CAB-4172-9392-5F9BF60AC75C'
+    key = 'A7833F58-4F15-424C-9BDA-8C0417EA8A9E'
+#     key = 'B9F26C13-0CAB-4172-9392-5F9BF60AC75C'
+#     key = '721E2F6B-A0C7-4D29-B473-89BD1B88F68C'
 
     headers = {'X-CoinAPI-Key' : key}
     date_today = date.today()
@@ -84,28 +98,49 @@ def get_data(coin: list) -> requests.Response.text:
 #* Saves json objects locally in csv_files
 #//// TODO: Implementation Required
 def save_data(data: list, filename: str) -> None:
+      """
+      def save_data(data: list, filename: str) -> None
+
+            saves api data in csv files
+      """
       list_of_dicts = json.loads(data)
-      with open(file=input_path+filename) as csv_file:
+      with open(file=input_path+filename, mode='w') as csv_file:
             dict_writer = csv.DictWriter(csv_file, fieldnames=col_names)
             dict_writer.writeheader()
             for dictionary in list_of_dicts:
                   dict_writer.writerow(dictionary)
 
-# TODO: Implementation Required
-def save_results(df: pd.DataFrame) -> None:
-      print(df)
-      plot = df.plot()
-      fig = plot.get_figure()
-      fig.savefig(f"{output_path}result.png")
+# TODO: Implementation Required (Not Important for this project)
+class Save_Results(beam.DoFn):
+      def process(self, element):
+            yield element
 
-# TODO: Implement Pipeline logic
+#//// TODO: Implement Pipeline logic
 def run(OPTIONS: PipelineOptions, filename: str) -> None:
-      
+      """
+      def run(OPTIONS: PipelineOptions, filename: str) -> None
+
+            run pipeline logic
+      """      
       with beam.Pipeline(options=OPTIONS) as P:
 
-            csv_file = P | "Read CSV files" >> read_csv(input_path+filename)
-            df = to_dataframe(pcoll=csv_file)
-            (df | "Save results" >> beam.Map(save_results()))               
+            dataframe = P | "Read CSV files" >> read_csv(input_path+filename)
+            # dataframe.plot() #! Plotting tools not supported yet
+            dataframe = dataframe[["time_close", "rate_close"]]
+            dataframe.to_csv(output_path+filename)
+            
+            if raw_table_name:
+                  (
+                        dataframe 
+                        | to_pcollection(include_indexes=True)
+                        # | "Some Operation" >> beam.ParDo(Save_Results()) #* don't need any to perform any operation for this project
+                        | "Write to Sink" >> beam.io.WriteToBigQuery(
+                              table=raw_table_name, 
+                              schema=col_names, 
+                              write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND,
+                              create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED,
+                              )
+                  )          
 
 def main() -> None:
       try:
